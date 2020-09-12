c75057820b1fc8d9176a1d60ebef81d93706f491
joeymichaelmartinez <joeymichaelmartinez@gmail.com>
2020-09-11 20:42:41 -0700

    initial commit, have projectiles working, with collision and a moving character

---
+++ BeatleBros/Assets/scripts/Bullet.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Bullet : MonoBehaviour
+{
+     public GameObject hitEffect;
+
+    void OnCollisionEnter2D(Collision2D collisionInfo)
+    {
+        Vector3 explosionPosition = transform.position;
+        // explosionPosition[1] = 
+        // Debug.Log(explosionPosition);
+        GameObject effect= Instantiate(hitEffect, transform.position, Quaternion.identity);
+        Destroy(effect, .25f);
+        Destroy(gameObject);
+    }
+}
